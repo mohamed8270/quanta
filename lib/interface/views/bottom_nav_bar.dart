e9bbe2ee813cba1e2319a8e3853b850d0747d6a4
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-05-26 23:54:25 +0530

    bottom nav bar updated

--- lib/interface/views/bottom_nav_bar.dart
+++ lib/interface/views/bottom_nav_bar.dart
@@ -17,6 +17,7 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final navigateControl = Get.put(NavigationController());
+
     return Scaffold(
       bottomNavigationBar: Obx(
         () => Container(
@@ -45,35 +46,79 @@ class BottomNavBar extends StatelessWidget {
               );
             })),
             child: NavigationBar(
-              height: 65,
+              height: 55,
               elevation: 0,
               backgroundColor: qwhite,
               selectedIndex: navigateControl.selectedIndex.value,
               onDestinationSelected: (index) =>
                   navigateControl.selectedIndex.value = index,
-              indicatorColor: qblack.withOpacity(0.035),
+              indicatorColor: qwhite,
               destinations: [
-                NavigationDestination(
-                  icon: StaticClass().outlineSvg(StaticClass.homeOutline),
-                  selectedIcon: StaticClass().filledSvg(StaticClass.homeFilled),
-                  label: 'Home',
+                Column(
+                  children: [
+                    BottomTopBox(
+                      isSelected: navigateControl.selectedIndex.value == 0,
+                    ),
+                    SizedBox(
+                      height: 48.5,
+                      child: NavigationDestination(
+                        icon: StaticClass().outlineSvg(StaticClass.homeOutline),
+                        selectedIcon:
+                            StaticClass().filledSvg(StaticClass.homeFilled),
+                        label: 'Home',
+                      ),
+                    ),
+                  ],
                 ),
-                NavigationDestination(
-                  icon: StaticClass().outlineSvg(StaticClass.amazonOutline),
-                  selectedIcon: SvgPicture.asset(StaticClass.amazonFilled,
-                      height: 24, width: 24),
-                  label: 'Products',
+                Column(
+                  children: [
+                    BottomTopBox(
+                      isSelected: navigateControl.selectedIndex.value == 1,
+                    ),
+                    SizedBox(
+                      height: 48.5,
+                      child: NavigationDestination(
+                        icon:
+                            StaticClass().outlineSvg(StaticClass.amazonOutline),
+                        selectedIcon: SvgPicture.asset(StaticClass.amazonFilled,
+                            height: 24, width: 24),
+                        label: 'Products',
+                      ),
+                    ),
+                  ],
                 ),
-                NavigationDestination(
-                  icon: StaticClass().outlineSvg(StaticClass.saleOutline),
-                  selectedIcon: StaticClass().filledSvg(StaticClass.saleFilled),
-                  label: 'Deals',
+                Column(
+                  children: [
+                    BottomTopBox(
+                      isSelected: navigateControl.selectedIndex.value == 2,
+                    ),
+                    SizedBox(
+                      height: 48.5,
+                      child: NavigationDestination(
+                        icon: StaticClass().outlineSvg(StaticClass.saleOutline),
+                        selectedIcon:
+                            StaticClass().filledSvg(StaticClass.saleFilled),
+                        label: 'Deals',
+                      ),
+                    ),
+                  ],
                 ),
-                NavigationDestination(
-                  icon: StaticClass().outlineSvg(StaticClass.profileOutline),
-                  selectedIcon:
-                      StaticClass().filledSvg(StaticClass.profileFilled),
-                  label: 'Profile',
+                Column(
+                  children: [
+                    BottomTopBox(
+                      isSelected: navigateControl.selectedIndex.value == 3,
+                    ),
+                    SizedBox(
+                      height: 48.5,
+                      child: NavigationDestination(
+                        icon: StaticClass()
+                            .outlineSvg(StaticClass.profileOutline),
+                        selectedIcon:
+                            StaticClass().filledSvg(StaticClass.profileFilled),
+                        label: 'Profile',
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -83,6 +128,30 @@ class BottomNavBar extends StatelessWidget {
       body: Obx(
         () => navigateControl.screens[navigateControl.selectedIndex.value],
       ),
+    );
+  }
+}
+
+class BottomTopBox extends StatelessWidget {
+  const BottomTopBox({super.key, required this.isSelected, this.child});
+
+  final bool isSelected;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.vertical(bottom: Radius.circular(5)),
+        border: Border(
+          top: BorderSide(
+            width: 5,
+            color: isSelected ? qblack : qwhite,
+          ),
+        ),
+      ),
+      child: child,
     );
   }
 }
